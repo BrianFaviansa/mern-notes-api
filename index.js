b9d2ef7260d8fe881d48bc51d874c39b5fea5751
BrianFaviansa <brianfpd31@gmail.com>
2024-07-23 06:53:04 +0700

    feat: get all notes api

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
     if (!note) {
       return res.status(404).json({ error: true, message: "Note not found" });
     }
-    if (title) note.tile = title;
+    if (title) note.title = title;
     if (content) note.content = content;
     if (tags) note.tags = tags;
     if (isPinned) note.isPinned = isPinned;
@@ -172,6 +172,24 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
       error: false,
       note,
       message: "Note updated successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal server error",
+    });
+  }
+});
+
+//* Get All Notes
+app.get("/get-all-notes", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+  try {
+    const notes = await Note.find({ userId: user._id }).sort({ isPinned: -1 });
+    return res.json({
+      error: false,
+      notes,
+      message: "All notes retrieved successfully",
     });
   } catch (error) {
     return res.status(500).json({
