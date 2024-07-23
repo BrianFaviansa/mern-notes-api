68703b886c66fe81be75ca8adbb2d974c91f3e4e
BrianFaviansa <brianfpd31@gmail.com>
2024-07-23 20:34:16 +0700

    feat: search notes api

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ app.get("/get-user", authenticateToken, async (req, res) => {
       fullName: isUser.fullName,
       email: isUser.email,
       _id: isUser._id,
-      createdOn: isUser.createdOn
+      createdOn: isUser.createdOn,
     },
     message: "Get user successfull",
   });
@@ -258,6 +258,38 @@ app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
       error: false,
       note,
       message: "Note updated successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: "Internal server error",
+    });
+  }
+});
+
+//* Search Notes
+app.get("/search-notes/", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({
+      error: true,
+      message: "Search keyword is required",
+    });
+  }
+  try {
+    const matchingNotes = await Note.find({
+      userId: user._id,
+      $or: [
+        { title: { $regex: new RegExp(query, "i") } },
+        { content: { $regex: new RegExp(query, "i") } },
+      ],
+    });
+    return res.json({
+      error: false,
+      notes: matchingNotes,
+      message: "Notes matching the search query retrieved successfully",
     });
   } catch (error) {
     return res.status(500).json({
